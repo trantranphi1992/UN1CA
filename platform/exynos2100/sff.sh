e5686e304f99682a93c230483b93b952e63628e6
Fred <74693315+xfwdrev@users.noreply.github.com>
2025-12-24 00:02:20 +0800

    o1s/t2s/palette/r9s: init (#535)
    * prebuilts: samsung: r9sxxx: add blobs
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * platform: exynos2100: init
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * target: o1s: init
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * target: p3s: init
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * target: r9s: init
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * target: t2s: init
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * exynos2100: config: de-commonize flags
    
    Change-Id: Ic6f5e4195b8f7e16a46fac154296c6cb85f47660
    
    * exynos2100: floating_feature: de-commonize flags
    
    Change-Id: I650f00a799a05c270924f07721a58bdf091fbc98
    
    * palette: config: adjust flags
    
    Change-Id: I9a7a9413b257d86ce83f9ded0af998f826e28a38
    
    * r9s: config: adjust flags
    
    Change-Id: I4c4e0e2627909a0ad75be1acaa2d7b44d8668933
    
    * r9s: remove unnecessary files
    
    Change-Id: Ia702e2ee8294cfe703efab960831b324219d4f76
    
    * palette/r9s: adjust debloat list
    
    Change-Id: I4941630a8cbb6c47b3f57bea5f271f2cc776842e
    
    * r9s: overlay: minor adjustments
    
    Change-Id: I543cabe8e02e645bd131d50628669a41d734684a
    
    * palette: overlay: minor adjustments
    
    Change-Id: Iac822a25885741672a9deae59bb43e3126ee4e17
    
    * exynos2100: stock_blobs>blobs, minor adjustments
    
    Change-Id: I1e049fd4573b4511cd2dbedc21f4875ca05c56bd
    
    * exynos2100: camera: clean up
    
    Change-Id: I4470e3211cc20d41f8548a9d0bf7feb5046cb5f9
    
    * prebuilts: samsung: r9sxxx: clean up
    
    Change-Id: I25cba18c711767c6ef01865942753f6c55c17d59
    
    * github-actions: blobs: match r9s config
    
    Change-Id: I3403cad1448255ced464a53a7862e05732f8e43e
    
    * github-actions: build: add S21 series
    
    Change-Id: I10538d8773a28203dbc2eda87563578dc955f92e
    
    * palette/r9s: patches: edge_lighting: minor adjustments
    
    Change-Id: I5942a152e09a489565e8508b310786c51efda094
    
    * exynos2100: stock_props: init
    
    Change-Id: I6e469b9800f3cb453a0fc7fd004f4ad5b137d1fa
    
    * palette: vintf: adapt to A16
    
    Change-Id: I15085895d673b8cd6a767b9cefdd019cdcf17539
    
    * exynos2100: blobs: add additional blobs
    
    Change-Id: I2665f0b662df60e2f67c1af352711b528f0bd26e
    
    * palette: floating_feature: enable AOD live clock
    
    Change-Id: I6b0dce1512c86daa3662e07a95345456d8c5da48
    
    * palette/r9s: installer: init customize.sh, install-end.edify
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * palette/r9s: installer: install-end.edify: minor adjustments
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * palette/r9s: installer: minor adjustments
    
    Change-Id: I08162b6b20418fcf670e1a8c2472e5f14b195a4e
    
    * .gitattributes: minor adjustments
    
    Change-Id: I2dcee46be59ef37239ea2e89e164643b8a4b7550
    
    * r9s: patches: vendor: minor adjustments
    
    Change-Id: Iec68c794900a47880e620921dff3c3e5cb2f6f36
    
    * p3s/r9s: installer: minor adjustments
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * palette/r9s: installer: adjust download link
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * platform: exynos2100: krnl: update URL
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    
    * exynos2100: patches: chiclet: minor adjustments
    
    Change-Id: I1c559a95e2aee2bad7245a8c9dca1f8e0324187a
    
    ---------
    
    Signed-off-by: xfwdrev <frignacio16@gmail.com>
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>

---
+++ platform/exynos2100/sff.sh
@@ -0,0 +1,24 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# SEC Floating Feature configuration file for Samsung Exynos 2100 devices (exynos2100)
+
+# ImageTagger version
+SEC_FLOATING_FEATURE_GALLERY_CONFIG_IMAGE_TAGGER_VERSION=V701
+
+# Enable 360 Lite Portrait
+SEC_FLOATING_FEATURE_GALLERY_CONFIG_LIVEFOCUS_EFFECT_DUAL_BOKEH=BLUR,EFFECT,BIGBOKEH,PORTRAIT,RELIGHT,REFOCUS,LIGHT360,PL_LITE_360
